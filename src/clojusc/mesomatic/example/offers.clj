f1ab9a6f7e56dd212d47cea67a9c2cee12056172
Duncan McGreggor <oubiwann@gmail.com>
2016-06-14 14:51:00 -0500

    Added resource tracking and short-circuiting.

--- src/clojusc/mesomatic/example/offers.clj
+++ src/clojusc/mesomatic/example/offers.clj
@@ -3,6 +3,7 @@
   (:require [clojure.tools.logging :as log]
             [clojure.string :as string]
             [clojusc.twig :refer [pprint]]
+            [clojusc.mesomatic.example.resources :as resources]
             [clojusc.mesomatic.example.task :as task]
             [clojusc.mesomatic.example.util :as util]))
 
@@ -18,29 +19,54 @@
 (defn hit-limits?
   ""
   [limits status]
+  (log/debug "limits:" limits)
+  (log/debug "status:" status)
   (if (or (< (:remaining-cpus status) (:cpus-per-task limits))
           (< (:remaining-mem status) (:mem-per-task limits)))
-    true
-    false))
+    (do
+      (log/debug "Hit resource limit.")
+      true)
+      false))
+
+(defn quit-loop?
+  ""
+  [limits status offers]
+  (or (hit-limits? limits status)
+      (empty? offers)))
+
+(defn update-status
+  ""
+  [status rsrcs]
+  (log/debug "Updating status with rsrcs:" rsrcs)
+  (-> status
+      (update :remaining-cpus (partial - (:cpus rsrcs)))
+      (update :remaining-mem (partial - (:mem rsrcs)))))
 
 (defn process-all
   ""
   [state data limits offers]
-  (loop [status {:remaining-cpus nil
-                 :remaining-mem nil}
+  (loop [status {:remaining-cpus 0
+                 :remaining-mem 0}
          index 1
          [offer & remaining-offers] offers
          tasks []]
-    ;; XXX - start
-    (log/debug "Offer:" (pprint offer))
-    (log/debug "Resources:" (pprint (:resources offer)))
-    ;; XXX - end
-    (if (or (hit-limits? limits status) (empty? remaining-offers))
-      tasks
-      (recur
-        ;; XXX update remaining resources
-        status
-        (inc index)
-        remaining-offers
-        (conj tasks
-              (process-one state data limits status index offer))))))
+    (let [rsrcs (resources/sum offer)
+          status (update-status status rsrcs)]
+      (if (quit-loop? limits status remaining-offers)
+        (do
+          (log/debug "Quitting loop ...")
+          tasks)
+        (do
+          (log/debug "Iterating offers ...")
+          (recur
+            ;; XXX update remaining resources
+            status
+            (inc index)
+            remaining-offers
+            (conj tasks
+                  (process-one state data limits status index offer))))))))
+
+(defn get-ids
+  ""
+  [offers]
+  (map :id offers))
