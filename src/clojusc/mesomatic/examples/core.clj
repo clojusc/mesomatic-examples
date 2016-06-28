9e721907c3f2414722a45faed6428f189f715a38
Duncan McGreggor <oubiwann@gmail.com>
2016-06-28 15:12:44 -0500

    Add help/usage support.

--- src/clojusc/mesomatic/examples/core.clj
+++ src/clojusc/mesomatic/examples/core.clj
@@ -6,7 +6,8 @@
             [clojusc.twig :as logger]
             [clojusc.mesomatic.examples.exception-framework :as ex-framework]
             [clojusc.mesomatic.examples.executor :as executor]
-            [clojusc.mesomatic.examples.framework :as framework])
+            [clojusc.mesomatic.examples.framework :as framework]
+            [clojusc.mesomatic.examples.util :as util])
   (:gen-class))
 
 (defn get-config
@@ -14,18 +15,35 @@
   []
   (:mesomatic-examples (lein-prj/read)))
 
+(defn usage
+  ""
+  []
+  (println)
+  (-> 'clojusc.mesomatic.examples.core
+      (util/get-docstring '-main)
+      (println)))
+
 (defn -main
-  "It is expected that this function be called from ``lein`` in the following
-  manner:
+  "It is expected that this function be called from ``lein`` in one of the
+  two following manners:
 
   ```
   $ lein mesomatic 127.0.0.1:5050 <task type>
+  ```
+
+  or
+
+  ```
+  $ lein mesomatic 127.0.0.1:5050 <task type> <task count>
   ```
 
   where ``<task-type>`` is one of:
 
   * ``executor``
   * ``framework``
+
+  and ``<task count>`` is an integer representing the number of times a task
+  will be run.
 
   That being said, only Mesos should call with the ``executor`` task type;
   calling humans will only call with the ``framework`` task type.
@@ -37,13 +55,19 @@
   :aliases {\"mesomatic\" [\"run\" \"-m\" \"mesomatic-examples.core\"]}
   ```
   "
-  [master task-type]
-  (let [cfg (get-config)]
-    (logger/set-level! (:log-namespaces cfg) (:log-level cfg))
-    (log/info "Running a mesomatic example!")
-    (log/debug "Using master:" master)
-    (log/debug "Got task-type:" task-type)
-    (condp = task-type
-      "executor" (executor/run master)
-      "framework" (framework/run master)
-      "exception-framework" (ex-framework/run master))))
+  ([flag]
+    (case flag
+      "--help" (usage)
+      "-h" (usage)))
+  ([master task-type]
+    (-main master task-type 5))
+  ([master task-type task-count]
+    (let [cfg (get-config)]
+      (logger/set-level! (:log-namespaces cfg) (:log-level cfg))
+      (log/info "Running a mesomatic example!")
+      (log/debug "Using master:" master)
+      (log/debug "Got task-type:" task-type)
+      (condp = task-type
+        "executor" (executor/run master)
+        "framework" (framework/run master task-count)
+        "exception-framework" (ex-framework/run master)))))
