5b315e9a64f8ae4f4172ba52e2de6495dcd1fac1
Duncan McGreggor <oubiwann@gmail.com>
2017-12-20 19:53:19 -0600

    Cleaned up profiles.

--- project.clj
+++ project.clj
@@ -4,28 +4,16 @@
   :license {
     :name "Eclipse Public License"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :exclusions [org.clojure/clojure]
   :dependencies [
     [clojusc/mesomatic "1.0.1-r0"]
     [clojusc/mesomatic-async "1.0.1-r0"]
     [clojusc/twig "0.3.2"]
     [com.stuartsierra/component "0.3.2"]
-    [org.clojure/clojure "1.9.0"]]
-  :plugins [
-    [lein-codox "0.10.3"]
-    [lein-simpleton "1.3.0"]]
+    [org.clojure/clojure "1.8.0"]]
   :mesomatic-examples {
     :log-namespaces [mesomatic.examples]
     :log-level :trace}
-  :codox {
-    :project {
-      :name "mesomatic-examples"
-      :description "A Clojure/Meosmatic port of the Mesos Java examples"}
-    :namespaces [#"^mesomatic.examples\."]
-    :output-path "docs/build"
-    :doc-paths ["docs"]
-    :metadata {
-      :doc/format :markdown
-      :doc "Documentation forthcoming"}}
   :profiles {
     :dev {
       :source-paths ["dev-resources/src"]
@@ -42,6 +30,20 @@
         [lein-kibit "0.1.6"]
         [lein-ltest "0.3.0-SNAPSHOT"]
         [venantius/yagni "0.1.4"]]}
+    :docs {
+      :plugins [
+        [lein-codox "0.10.3"]
+        [lein-simpleton "1.3.0"]]
+      :codox {
+        :project {
+          :name "mesomatic-examples"
+          :description "A Clojure/Meosmatic port of the Mesos Java examples"}
+        :namespaces [#"^mesomatic.examples\."]
+        :output-path "docs/build"
+        :doc-paths ["docs"]
+        :metadata {
+          :doc/format :markdown
+          :doc "Documentation forthcoming"}}}
     :ubercompile {
       :aot :all}
     :custom-repl {
@@ -55,11 +57,16 @@
                             "For more info run `lein mesomatic --help`\n")}
                 ^:pass-through-help
                 ["run" "-m" "mesomatic.examples.core"]
-    "repl" ["with-profile" "+custom-repl,+test" "repl"]
+    "repl" ["with-profile" "+custom-repl,+test" "do"
+      ["clean"]
+      ["repl"]]
     "ubercompile" ["with-profile" "+ubercompile" "compile"]
     "check-deps" ["with-profile" "+test" "ancient" "check" ":all"]
     "lint" ["with-profile" "+test" "kibit"]
     "test" ["with-profile" "+test" "ltest"]
+    "docs" ["with-profile" "+docs" "do"
+      ["clean"]
+      ["codox"]]
     "build-check" ["with-profile" "+test" "do"
       ;["check-deps"]
       ["lint"]
@@ -67,4 +74,5 @@
       ["clean"]
       ["uberjar"]
       ["clean"]
-      ["test"]]})
+      ["test"]
+      ["docs"]]})
