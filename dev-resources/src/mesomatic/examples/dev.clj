287f589ac3c077a5dee1dfc62c3451b98fce514b
Duncan McGreggor <oubiwann@gmail.com>
2017-12-20 19:53:35 -0600

    Removed old requires.

--- dev-resources/src/mesomatic/examples/dev.clj
+++ dev-resources/src/mesomatic/examples/dev.clj
@@ -1,18 +1,17 @@
 (ns mesomatic.examples.dev
   "Dev namespace with requires, constants, and functions setup and ready to
   go."
-  (:require [clojure.core.async :as a :refer [chan <! go]]
-            [clojure.tools.logging :as log]
-            [mesomatic.async.executor :as async-executor]
-            [mesomatic.async.scheduler :as async-scheduler]
-            [mesomatic.executor :as executor]
-            [mesomatic.scheduler :as scheduler]
-            [mesomatic.types :as types]
-            [clojusc.twig :as logger]
-            [mesomatic.examples.core :as core]
-            [mesomatic.examples.executor :as example-executor]
-            [mesomatic.examples.framework :as example-framework]
-            [mesomatic.examples.util :as util]))
+  (:require
+    [clojure.core.async :as a :refer [chan <! go]]
+    [clojure.tools.logging :as log]
+    [clojusc.twig :as logger]
+    [mesomatic.async.executor :as async-executor]
+    [mesomatic.async.scheduler :as async-scheduler]
+    [mesomatic.examples.core :as core]
+    [mesomatic.examples.util :as util]
+    [mesomatic.executor :as executor]
+    [mesomatic.scheduler :as scheduler]
+    [mesomatic.types :as types]))
 
 ;;; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ;;; Dev Constants
