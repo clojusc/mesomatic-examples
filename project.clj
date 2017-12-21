7e6959e0d5c4a17045c32fa6d529280f214980a3
Duncan McGreggor <oubiwann@gmail.com>
2017-12-20 19:42:48 -0600

    Added profiles and aliases.

--- project.clj
+++ project.clj
@@ -30,9 +30,41 @@
   :profiles {
     :dev {
       :source-paths ["dev-resources/src"]
-      :repl-options {:init-ns mesomatic.examples.dev}}}
+      :repl-options {:init-ns mesomatic.examples.dev}}
+    :test {
+      :exclusions [org.clojure/clojure]
+      :dependencies [
+        [clojusc/ltest "0.3.0-SNAPSHOT"]]
+      :plugins [
+        [jonase/eastwood "0.2.5"]
+        [lein-ancient "0.6.15"]
+        [lein-bikeshed "0.5.0"]
+        [lein-kibit "0.1.6"]
+        [lein-ltest "0.3.0-SNAPSHOT"]
+        [venantius/yagni "0.1.4"]]}
+    :ubercompile {
+      :aot :all}
+    :custom-repl {
+      :repl-options {
+        :init-ns mesomatic.examples.dev
+        :prompt ~#(str "\u001B[35m[\u001B[34m"
+                       %
+                       "\u001B[35m]\u001B[33m λ\u001B[m=> ")}}}
   :aliases {
     "mesomatic" ^{:doc (str "Command line interface for mesomatic-examples. "
                             "For more info run `lein mesomatic --help`\n")}
                 ^:pass-through-help
-                ["run" "-m" "mesomatic.examples.core"]})
+                ["run" "-m" "mesomatic.examples.core"]
+    "repl" ["with-profile" "+custom-repl,+test" "repl"]
+    "ubercompile" ["with-profile" "+ubercompile" "compile"]
+    "check-deps" ["with-profile" "+test" "ancient" "check" ":all"]
+    "lint" ["with-profile" "+test" "kibit"]
+    "test" ["with-profile" "+test" "ltest"]
+    "build-check" ["with-profile" "+test" "do"
+      ;["check-deps"]
+      ["lint"]
+      ["ubercompile"]
+      ["clean"]
+      ["uberjar"]
+      ["clean"]
+      ["test"]]})
