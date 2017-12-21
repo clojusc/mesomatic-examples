5aebb66e6446cf8f9eb59984bf6205ec24ad0a86
Duncan McGreggor <oubiwann@gmail.com>
2017-12-20 19:39:42 -0600

    Cleanup.

--- project.clj
+++ project.clj
@@ -4,26 +4,23 @@
   :license {
     :name "Eclipse Public License"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [clojusc/twig "0.2.6"]
-                 [spootnik/mesomatic "1.0.1-r0"]
-                 [spootnik/mesomatic-async "1.0.1-r0"]
-                 [leiningen-core "2.7.1"]]
-  :plugins [[lein-codox "0.10.1"]
-            [lein-simpleton "1.3.0"]]
-  :aliases {"mesomatic"
-            ^{:doc "Command line interface for mesomatic-examples.
-           For more info run `lein mesomatic --help`\n"}
-            ^:pass-through-help
-            ["run" "-m" "mesomatic.examples.core"]}
+  :dependencies [
+    [clojusc/twig "0.2.6"]
+    [com.stuartsierra/component "0.3.1"]
+    [leiningen-core "2.7.1"]
+    [org.clojure/clojure "1.8.0"]
+    [spootnik/mesomatic "1.0.1-r0"]
+    [spootnik/mesomatic-async "1.0.1-r0"]]
+  :plugins [
+    [lein-codox "0.10.1"]
+    [lein-simpleton "1.3.0"]]
   :mesomatic-examples {
     :log-namespaces [mesomatic.examples]
     :log-level :trace}
   :codox {
     :project {
       :name "mesomatic-examples"
-      :description "A Clojure Port of the Mesos Java Examples"}
+      :description "A Clojure/Meosmatic port of the Mesos Java examples"}
     :namespaces [#"^mesomatic.examples\."]
     :output-path "docs/build"
     :doc-paths ["docs"]
@@ -33,4 +30,9 @@
   :profiles {
     :dev {
       :source-paths ["dev-resources/src"]
-      :repl-options {:init-ns mesomatic.examples.dev}}})
+      :repl-options {:init-ns mesomatic.examples.dev}}}
+  :aliases {
+    "mesomatic" ^{:doc (str "Command line interface for mesomatic-examples. "
+                            "For more info run `lein mesomatic --help`\n")}
+                ^:pass-through-help
+                ["run" "-m" "mesomatic.examples.core"]})
